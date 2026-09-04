8fc9690c770d38a7c19d69fe4569ff2e73b370df
Louis <git@louiszn.fyi>
2026-09-04 20:32:07 +0700

    feat: add home page :3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	integrations: [mdx()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [mdx(), icon()],
 });
