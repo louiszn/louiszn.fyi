018dd76d3cdcc151421bac3ddfe8bcb1b4cfbaa1
Louis <git@louiszn.fyi>
2026-09-04 12:37:04 +0700

    chore: init project

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [mdx()],
+});
